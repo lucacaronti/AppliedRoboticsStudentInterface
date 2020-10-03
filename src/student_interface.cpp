50e234f5d0a1e7862ffe7d76d378d6b076264a5a
lucacaronti <carontiluca@gmail.com>
2020-10-03 12:17:52 +0200

    started the implementation of extrinsicCalib function, TODO: implement 'findPlaneTransform' in order to test it

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -1,7 +1,9 @@
 #include "student_image_elab_interface.hpp"
 #include "student_planning_interface.hpp"
 
-
+#include <opencv2/opencv.hpp>
+#include <experimental/filesystem>
+#include <atomic>
 #include <stdexcept>
 #include <sstream>
 namespace student {
@@ -39,8 +41,135 @@ namespace student {
     //throw std::logic_error( "STUDENT FUNCTION - IMAGE LISTENER - NOT CORRECTLY IMPLEMENTED" );
   }
 
+  /****************************************************/
+  /**************** EXTRINSIC CALIB *******************/
+  /****************************************************/
+
+  bool readCSV(std::vector<std::string>& string_vector, std::fstream& file, int& elements_for_line){
+    std::string line;
+    elements_for_line = 0;
+    // Read data, line by line
+    while(std::getline(file, line)){
+      
+      int elements_for_line_tmp = 0;
+      // Create a stringstream of the current line
+      std::stringstream ss(line);
+
+      // Extract each column name
+      while(std::getline(ss, line, ',')){
+          elements_for_line_tmp ++;
+          string_vector.push_back(line);
+      }
+      if(elements_for_line == 0){
+        elements_for_line = elements_for_line_tmp;
+      }else if(elements_for_line_tmp != elements_for_line){
+        std::cout<<"Error in CSV format"<<std::endl;
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+
+struct mouseCallbackUserData_t{
+  std::vector<cv::Point2f>* points;
+  int points_counter;
+  int num_points_to_take;
+  std::atomic<bool> done;
+};
+
+void CallBackFunc(int event, int x, int y, int flags, void* userdata){
+  if  ( event == cv::EVENT_LBUTTONDOWN ){
+    mouseCallbackUserData_t* mouseCallbackUserData = (struct mouseCallbackUserData_t*)userdata;
+    mouseCallbackUserData->points->emplace_back(x,y);
+    mouseCallbackUserData->points_counter++;
+    if(mouseCallbackUserData->points_counter == mouseCallbackUserData->num_points_to_take){
+        mouseCallbackUserData->done.store(true);
+    }
+  }
+}
+
+void selectNpoints(const cv::Mat& image, std::vector<cv::Point2f>& allPoints, int num_points_to_take){
+
+  mouseCallbackUserData_t mouseCallbackUserData;
+  mouseCallbackUserData.points = &allPoints;
+  mouseCallbackUserData.num_points_to_take = num_points_to_take;
+
+  //Create a window
+  cv::namedWindow("Image", 1);
+
+  //set the callback function for any mouse event
+  cv::setMouseCallback("Image", CallBackFunc, &mouseCallbackUserData);
+
+  //show the image
+  cv::imshow("Image", image);
+
+  while (!mouseCallbackUserData.done.load()) {
+    cv::waitKey(500);
+  }
+}
+
   bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - NOT IMPLEMENTED" );   
+    std::cout<<"[DEBUG] entered in extrinsicCalib"<<std::endl;
+    // Define the configuration file name
+    std::string extrinsicCalibFileName = "extrinsicCalibData.csv";
+    std::string extrinsicCalibFilePath = config_folder + "/" + extrinsicCalibFileName;
+
+    std::vector<cv::Point2f> image_points; // Define image points type
+    std::cout<<"[DEBUG] try to find conf file: "<< extrinsicCalibFilePath <<std::endl;
+    Check if configuration file already exists
+    if(std::experimental::filesystem::exists(extrinsicCalibFilePath)){
+      std::cout<<"[DEBUG] found conf file: "<< extrinsicCalibFilePath <<std::endl;
+      std::fstream confFileIn; // Define configuration file to read
+
+      confFileIn.open(extrinsicCalibFilePath, std::ios::in);
+
+      if(!confFileIn.is_open()) throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - CAN'T OPEN '" + extrinsicCalibFilePath + "' FILE");
+      
+      std::vector<std::string> allLines;
+      int num_elements_for_line;
+
+      bool ret = readCSV(allLines, confFileIn, num_elements_for_line);
+      if(ret == false || num_elements_for_line != 2){
+        confFileIn.close();
+        throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - ERROR IN READING CSV FILE");
+      }else{
+
+        if(allLines[0].compare("x") == 0 && allLines[1].compare("y") == 0){
+          std::vector<std::string>::iterator its = allLines.begin(); // Define an iterator for allLines
+          its += 2; // waste the first line
+          for(its; its != allLines.end(); its++){
+            double x, y;
+            x = std::stod(*its);
+            its++;
+            y = std::stod(*its);
+            image_points.emplace_back(x,y); // Insert coordinates into points vector
+          }
+        } 
+      }
+      confFileIn.close();
+
+    }else{
+      std::cout<<"[DEBUG] NOT found conf file: "<< extrinsicCalibFilePath <<std::endl;
+      selectNpoints(img_in, image_points, 4);
+      std::experimental::filesystem::create_directories(config_folder);
+
+      std::fstream confFileOut; // Define configuration file to read
+      confFileOut.open(extrinsicCalibFilePath, std::ios::out);
+      if(!confFileOut.is_open()) throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - CAN'T CREATE '" + extrinsicCalibFilePath + "' FILE");
+
+      confFileOut << "x,y\n";
+
+      std::vector<cv::Point2f>::iterator itp;
+      for(itp = image_points.begin(); itp != image_points.end(); itp++){
+        confFileOut << itp->x << "," << itp->y << "\n";
+      }
+      confFileOut.close();
+    // }
+    cv::Mat dist_coeffs = cv::Mat1d::zeros(1,4);
+    bool ok = cv::solvePnP(object_points, image_points, camera_matrix, dist_coeffs, rvec, tvec);
+    return ok;
   }
 
   void imageUndistort(const cv::Mat& img_in, cv::Mat& img_out, 
