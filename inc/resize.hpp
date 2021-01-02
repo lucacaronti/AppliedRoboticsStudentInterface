5336b2bf1aed327c4fea4d579f16398a8efc0b43
riccardoscilla <66726419+riccardoscilla@users.noreply.github.com>
2021-01-02 13:07:10 -0500

    finished resize of borders and arena (problem with sbmp erase points)

--- inc/resize.hpp
+++ inc/resize.hpp
@@ -5,3 +5,4 @@
 #include "clipper.hpp"
 
 std::vector<Polygon> resizeObstacles(const std::vector<Polygon> &obstacles, double robotSize);
+Polygon resizeBorders(const Polygon &borders, double robotSize);
--- src/resize.cpp
+++ src/resize.cpp
@@ -1,7 +1,12 @@
 #include "resize.hpp"
 
+#define INT_ROUND 1000.
+
 std::vector<Polygon> resizeObstacles(const std::vector<Polygon> &obstacles, double robotSize){
     std::vector<Polygon> newObstacles;
+
+    ClipperLib::Clipper cl;
+    ClipperLib::Paths mergedObstacles;
 
     for (const Polygon &obstacle : obstacles) {
         ClipperLib::Path clipperSrcObstacle;
@@ -10,26 +15,61 @@ std::vector<Polygon> resizeObstacles(const std::vector<Polygon> &obstacles, doub
 
         // transform Polygon to Clipper
         for (const auto &point : obstacle) {
-            clipperSrcObstacle << ClipperLib::IntPoint(point.x * 1000.0, point.y * 1000.0);
+            clipperSrcObstacle << ClipperLib::IntPoint(point.x * INT_ROUND, point.y * INT_ROUND);
         }
 
         ClipperLib::ClipperOffset co;
         co.AddPath(clipperSrcObstacle, ClipperLib::jtSquare, ClipperLib::etClosedPolygon);
         co.Execute(clipperNewObstacle, robotSize);
 
-        Polygon newPolygon;
-        // transform Clipper to Polygon
-        for(const ClipperLib::Path &path: clipperNewObstacle){
-            for(const ClipperLib::IntPoint &pt: path){
-                double x = pt.X / 1000.0;
-                double y = pt.Y / 1000.0;
-                newPolygon.emplace_back(x, y);
-            }
-        }
+        cl.AddPaths(clipperNewObstacle, ClipperLib::ptSubject, true);
 
+    }
+
+    cl.Execute(ClipperLib::ctUnion, mergedObstacles, ClipperLib::pftNonZero, ClipperLib::pftNonZero);
+
+    for(const ClipperLib::Path &path : mergedObstacles){
+        Polygon newPolygon;
+        for(const ClipperLib::IntPoint &pt: path){
+            double x = pt.X / INT_ROUND;
+            double y = pt.Y / INT_ROUND;
+            newPolygon.emplace_back(x, y);
+        }
         newObstacles.emplace_back(newPolygon);
     }
-    
 
     return newObstacles;
 }
+
+Polygon resizeBorders(const Polygon &borders, double robotSize){
+    Polygon newBorders;
+
+    ClipperLib::Path clipperSrcBorder;
+    ClipperLib::Paths clipperNewBorder;
+
+    // transform Polygon to Clipper
+    for (const auto &point : borders) {
+        clipperSrcBorder << ClipperLib::IntPoint(point.x * INT_ROUND, point.y * INT_ROUND);
+    }
+
+    ClipperLib::ClipperOffset co;
+    co.AddPath(clipperSrcBorder, ClipperLib::jtSquare, ClipperLib::etClosedPolygon);
+    co.Execute(clipperNewBorder, -robotSize);
+
+    for(const ClipperLib::Path &path : clipperNewBorder){
+        for(const ClipperLib::IntPoint &pt: path){
+            double x = pt.X / INT_ROUND;
+            double y = pt.Y / INT_ROUND;
+            newBorders.emplace_back(x, y);
+        }
+    }
+
+    Polygon orderedBorders;
+    orderedBorders.emplace_back(newBorders[2]);
+    orderedBorders.emplace_back(newBorders[3]);
+    orderedBorders.emplace_back(newBorders[0]);
+    orderedBorders.emplace_back(newBorders[1]);
+
+    return orderedBorders;
+}
+
